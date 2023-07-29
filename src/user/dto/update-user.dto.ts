ad302197c94a52ccb9dd8501e81cf97c95886ac8
adnan23062000 <bsse1131@iit.du.ac.bd>
2023-07-29 14:25:06 +0600

    complete user crud

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    // updateUserDTO te attribute gula na dileo chole as eita createUserDTO theke inherit kore
+    firstName: string;
+    lastName: string;
+    age: number;
+}
